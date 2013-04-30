d96d68f475e56d470a78636ce56d4e2de9707adc
kaczuss <kaczan87@gmail.com>
2013-04-30 10:05:00 +0200

    lepsze skrypty do odpalania

--- run_g40_all_prob.bat
+++ run_g40_all_prob.bat
@@ -13,45 +13,44 @@ java -jar target/scheduling-geo-0.0.1-SNAPSHOT-jar-with-dependencies.jar -f=src/
 set PROB=0.2
 set PROB_DIR_NAME=0_2
 
-java -jar target/scheduling-geo-0.0.1-SNAPSHOT-jar-with-dependencies.jar -f=src/main/resources/%ALGORITHM%.txt -bestMeanWorst=G:/dokumenty/magisterka-geo-szer/raport/%ALGORITHM%/%date%/%PROB_DIR_NAME%/iterations_best_%ALGORITHM%.csv -currentBestWorst=G:/dokumenty/magisterka-geo-szer/raport/%ALGORITHM%/%date%/%PROB_DIR_NAME%/gauss_best_worst_{0}_%ALGORITHM%.csv -iterToBest=G:/dokumenty/magisterka-geo-szer/raport/%ALGORITHM%/%date%/%PROB_DIR_NAME%/iterations_to_best.csv -best=%BEST% -ri=%RUN_ITERATIONS% -i=%ITERATIONS% -prob=%PROB%  > .run/%ALGORITHM%_%date%_prob_%PROB_DIR_NAME%.out.txt
-
+java -jar target/scheduling-geo-0.0.1-SNAPSHOT-jar-with-dependencies.jar -f=src/main/resources/%ALGORITHM%.txt -bestMeanWorst=G:/dokumenty/magisterka-geo-szer/raport/%ALGORITHM%/%date%/%PROB_DIR_NAME%/iterations_best_%ALGORITHM%.csv -currentBestWorst=G:/dokumenty/magisterka-geo-szer/raport/%ALGORITHM%/%date%/%PROB_DIR_NAME%/gauss_best_worst_{0}_%ALGORITHM%.csv -iterToBest=G:/dokumenty/magisterka-geo-szer/raport/%ALGORITHM%/%date%/%PROB_DIR_NAME%/iterations_to_best.csv -distribution=G:/dokumenty/magisterka-geo-szer/raport/%ALGORITHM%/%date%/%PROB_DIR_NAME%/distribution.csv -best=%BEST% -ri=%RUN_ITERATIONS% -i=%ITERATIONS% -prob=%PROB%  > .run/%ALGORITHM%_%date%_prob_%PROB_DIR_NAME%.out.txt
 
 set PROB=0.5
 set PROB_DIR_NAME=0_5
 
-java -jar target/scheduling-geo-0.0.1-SNAPSHOT-jar-with-dependencies.jar -f=src/main/resources/%ALGORITHM%.txt -bestMeanWorst=G:/dokumenty/magisterka-geo-szer/raport/%ALGORITHM%/%date%/%PROB_DIR_NAME%/iterations_best_%ALGORITHM%.csv -currentBestWorst=G:/dokumenty/magisterka-geo-szer/raport/%ALGORITHM%/%date%/%PROB_DIR_NAME%/gauss_best_worst_{0}_%ALGORITHM%.csv -iterToBest=G:/dokumenty/magisterka-geo-szer/raport/%ALGORITHM%/%date%/%PROB_DIR_NAME%/iterations_to_best.csv -best=%BEST% -ri=%RUN_ITERATIONS% -i=%ITERATIONS% -prob=%PROB%  > .run/%ALGORITHM%_%date%_prob_%PROB_DIR_NAME%.out.txt
+java -jar target/scheduling-geo-0.0.1-SNAPSHOT-jar-with-dependencies.jar -f=src/main/resources/%ALGORITHM%.txt -bestMeanWorst=G:/dokumenty/magisterka-geo-szer/raport/%ALGORITHM%/%date%/%PROB_DIR_NAME%/iterations_best_%ALGORITHM%.csv -currentBestWorst=G:/dokumenty/magisterka-geo-szer/raport/%ALGORITHM%/%date%/%PROB_DIR_NAME%/gauss_best_worst_{0}_%ALGORITHM%.csv -iterToBest=G:/dokumenty/magisterka-geo-szer/raport/%ALGORITHM%/%date%/%PROB_DIR_NAME%/iterations_to_best.csv -distribution=G:/dokumenty/magisterka-geo-szer/raport/%ALGORITHM%/%date%/%PROB_DIR_NAME%/distribution.csv -best=%BEST% -ri=%RUN_ITERATIONS% -i=%ITERATIONS% -prob=%PROB%  > .run/%ALGORITHM%_%date%_prob_%PROB_DIR_NAME%.out.txt
 
 
 set PROB=0.8
 set PROB_DIR_NAME=0_8
 
-java -jar target/scheduling-geo-0.0.1-SNAPSHOT-jar-with-dependencies.jar -f=src/main/resources/%ALGORITHM%.txt -bestMeanWorst=G:/dokumenty/magisterka-geo-szer/raport/%ALGORITHM%/%date%/%PROB_DIR_NAME%/iterations_best_%ALGORITHM%.csv -currentBestWorst=G:/dokumenty/magisterka-geo-szer/raport/%ALGORITHM%/%date%/%PROB_DIR_NAME%/gauss_best_worst_{0}_%ALGORITHM%.csv -iterToBest=G:/dokumenty/magisterka-geo-szer/raport/%ALGORITHM%/%date%/%PROB_DIR_NAME%/iterations_to_best.csv -best=%BEST% -ri=%RUN_ITERATIONS% -i=%ITERATIONS% -prob=%PROB%  > .run/%ALGORITHM%_%date%_prob_%PROB_DIR_NAME%.out.txt
+java -jar target/scheduling-geo-0.0.1-SNAPSHOT-jar-with-dependencies.jar -f=src/main/resources/%ALGORITHM%.txt -bestMeanWorst=G:/dokumenty/magisterka-geo-szer/raport/%ALGORITHM%/%date%/%PROB_DIR_NAME%/iterations_best_%ALGORITHM%.csv -currentBestWorst=G:/dokumenty/magisterka-geo-szer/raport/%ALGORITHM%/%date%/%PROB_DIR_NAME%/gauss_best_worst_{0}_%ALGORITHM%.csv -iterToBest=G:/dokumenty/magisterka-geo-szer/raport/%ALGORITHM%/%date%/%PROB_DIR_NAME%/iterations_to_best.csv -distribution=G:/dokumenty/magisterka-geo-szer/raport/%ALGORITHM%/%date%/%PROB_DIR_NAME%/distribution.csv -best=%BEST% -ri=%RUN_ITERATIONS% -i=%ITERATIONS% -prob=%PROB%  > .run/%ALGORITHM%_%date%_prob_%PROB_DIR_NAME%.out.txt
 
 
 set PROB=1.0
 set PROB_DIR_NAME=1_0
 
-java -jar target/scheduling-geo-0.0.1-SNAPSHOT-jar-with-dependencies.jar -f=src/main/resources/%ALGORITHM%.txt -bestMeanWorst=G:/dokumenty/magisterka-geo-szer/raport/%ALGORITHM%/%date%/%PROB_DIR_NAME%/iterations_best_%ALGORITHM%.csv -currentBestWorst=G:/dokumenty/magisterka-geo-szer/raport/%ALGORITHM%/%date%/%PROB_DIR_NAME%/gauss_best_worst_{0}_%ALGORITHM%.csv -iterToBest=G:/dokumenty/magisterka-geo-szer/raport/%ALGORITHM%/%date%/%PROB_DIR_NAME%/iterations_to_best.csv -best=%BEST% -ri=%RUN_ITERATIONS% -i=%ITERATIONS% -prob=%PROB%  > .run/%ALGORITHM%_%date%_prob_%PROB_DIR_NAME%.out.txt
+java -jar target/scheduling-geo-0.0.1-SNAPSHOT-jar-with-dependencies.jar -f=src/main/resources/%ALGORITHM%.txt -bestMeanWorst=G:/dokumenty/magisterka-geo-szer/raport/%ALGORITHM%/%date%/%PROB_DIR_NAME%/iterations_best_%ALGORITHM%.csv -currentBestWorst=G:/dokumenty/magisterka-geo-szer/raport/%ALGORITHM%/%date%/%PROB_DIR_NAME%/gauss_best_worst_{0}_%ALGORITHM%.csv -iterToBest=G:/dokumenty/magisterka-geo-szer/raport/%ALGORITHM%/%date%/%PROB_DIR_NAME%/iterations_to_best.csv -distribution=G:/dokumenty/magisterka-geo-szer/raport/%ALGORITHM%/%date%/%PROB_DIR_NAME%/distribution.csv -best=%BEST% -ri=%RUN_ITERATIONS% -i=%ITERATIONS% -prob=%PROB%  > .run/%ALGORITHM%_%date%_prob_%PROB_DIR_NAME%.out.txt
 
 
 set PROB=1.5
 set PROB_DIR_NAME=1_5
 
-java -jar target/scheduling-geo-0.0.1-SNAPSHOT-jar-with-dependencies.jar -f=src/main/resources/%ALGORITHM%.txt -bestMeanWorst=G:/dokumenty/magisterka-geo-szer/raport/%ALGORITHM%/%date%/%PROB_DIR_NAME%/iterations_best_%ALGORITHM%.csv -currentBestWorst=G:/dokumenty/magisterka-geo-szer/raport/%ALGORITHM%/%date%/%PROB_DIR_NAME%/gauss_best_worst_{0}_%ALGORITHM%.csv -iterToBest=G:/dokumenty/magisterka-geo-szer/raport/%ALGORITHM%/%date%/%PROB_DIR_NAME%/iterations_to_best.csv -best=%BEST% -ri=%RUN_ITERATIONS% -i=%ITERATIONS% -prob=%PROB%  > .run/%ALGORITHM%_%date%_prob_%PROB_DIR_NAME%.out.txt
+java -jar target/scheduling-geo-0.0.1-SNAPSHOT-jar-with-dependencies.jar -f=src/main/resources/%ALGORITHM%.txt -bestMeanWorst=G:/dokumenty/magisterka-geo-szer/raport/%ALGORITHM%/%date%/%PROB_DIR_NAME%/iterations_best_%ALGORITHM%.csv -currentBestWorst=G:/dokumenty/magisterka-geo-szer/raport/%ALGORITHM%/%date%/%PROB_DIR_NAME%/gauss_best_worst_{0}_%ALGORITHM%.csv -iterToBest=G:/dokumenty/magisterka-geo-szer/raport/%ALGORITHM%/%date%/%PROB_DIR_NAME%/iterations_to_best.csv -distribution=G:/dokumenty/magisterka-geo-szer/raport/%ALGORITHM%/%date%/%PROB_DIR_NAME%/distribution.csv -best=%BEST% -ri=%RUN_ITERATIONS% -i=%ITERATIONS% -prob=%PROB%  > .run/%ALGORITHM%_%date%_prob_%PROB_DIR_NAME%.out.txt
 
 set PROB=2.0
 set PROB_DIR_NAME=2_0
 
-java -jar target/scheduling-geo-0.0.1-SNAPSHOT-jar-with-dependencies.jar -f=src/main/resources/%ALGORITHM%.txt -bestMeanWorst=G:/dokumenty/magisterka-geo-szer/raport/%ALGORITHM%/%date%/%PROB_DIR_NAME%/iterations_best_%ALGORITHM%.csv -currentBestWorst=G:/dokumenty/magisterka-geo-szer/raport/%ALGORITHM%/%date%/%PROB_DIR_NAME%/gauss_best_worst_{0}_%ALGORITHM%.csv -iterToBest=G:/dokumenty/magisterka-geo-szer/raport/%ALGORITHM%/%date%/%PROB_DIR_NAME%/iterations_to_best.csv -best=%BEST% -ri=%RUN_ITERATIONS% -i=%ITERATIONS% -prob=%PROB%  > .run/%ALGORITHM%_%date%_prob_%PROB_DIR_NAME%.out.txt
+java -jar target/scheduling-geo-0.0.1-SNAPSHOT-jar-with-dependencies.jar -f=src/main/resources/%ALGORITHM%.txt -bestMeanWorst=G:/dokumenty/magisterka-geo-szer/raport/%ALGORITHM%/%date%/%PROB_DIR_NAME%/iterations_best_%ALGORITHM%.csv -currentBestWorst=G:/dokumenty/magisterka-geo-szer/raport/%ALGORITHM%/%date%/%PROB_DIR_NAME%/gauss_best_worst_{0}_%ALGORITHM%.csv -iterToBest=G:/dokumenty/magisterka-geo-szer/raport/%ALGORITHM%/%date%/%PROB_DIR_NAME%/iterations_to_best.csv -distribution=G:/dokumenty/magisterka-geo-szer/raport/%ALGORITHM%/%date%/%PROB_DIR_NAME%/distribution.csv -best=%BEST% -ri=%RUN_ITERATIONS% -i=%ITERATIONS% -prob=%PROB%  > .run/%ALGORITHM%_%date%_prob_%PROB_DIR_NAME%.out.txt
 
 
 set PROB=5.0
 set PROB_DIR_NAME=5_0
 
-java -jar target/scheduling-geo-0.0.1-SNAPSHOT-jar-with-dependencies.jar -f=src/main/resources/%ALGORITHM%.txt -bestMeanWorst=G:/dokumenty/magisterka-geo-szer/raport/%ALGORITHM%/%date%/%PROB_DIR_NAME%/iterations_best_%ALGORITHM%.csv -currentBestWorst=G:/dokumenty/magisterka-geo-szer/raport/%ALGORITHM%/%date%/%PROB_DIR_NAME%/gauss_best_worst_{0}_%ALGORITHM%.csv -iterToBest=G:/dokumenty/magisterka-geo-szer/raport/%ALGORITHM%/%date%/%PROB_DIR_NAME%/iterations_to_best.csv -best=%BEST% -ri=%RUN_ITERATIONS% -i=%ITERATIONS% -prob=%PROB%  > .run/%ALGORITHM%_%date%_prob_%PROB_DIR_NAME%.out.txt
+java -jar target/scheduling-geo-0.0.1-SNAPSHOT-jar-with-dependencies.jar -f=src/main/resources/%ALGORITHM%.txt -bestMeanWorst=G:/dokumenty/magisterka-geo-szer/raport/%ALGORITHM%/%date%/%PROB_DIR_NAME%/iterations_best_%ALGORITHM%.csv -currentBestWorst=G:/dokumenty/magisterka-geo-szer/raport/%ALGORITHM%/%date%/%PROB_DIR_NAME%/gauss_best_worst_{0}_%ALGORITHM%.csv -iterToBest=G:/dokumenty/magisterka-geo-szer/raport/%ALGORITHM%/%date%/%PROB_DIR_NAME%/iterations_to_best.csv -distribution=G:/dokumenty/magisterka-geo-szer/raport/%ALGORITHM%/%date%/%PROB_DIR_NAME%/distribution.csv -best=%BEST% -ri=%RUN_ITERATIONS% -i=%ITERATIONS% -prob=%PROB%  > .run/%ALGORITHM%_%date%_prob_%PROB_DIR_NAME%.out.txt
 
 
 set PROB=8.0
 set PROB_DIR_NAME=8_0
 
-java -jar target/scheduling-geo-0.0.1-SNAPSHOT-jar-with-dependencies.jar -f=src/main/resources/%ALGORITHM%.txt -bestMeanWorst=G:/dokumenty/magisterka-geo-szer/raport/%ALGORITHM%/%date%/%PROB_DIR_NAME%/iterations_best_%ALGORITHM%.csv -currentBestWorst=G:/dokumenty/magisterka-geo-szer/raport/%ALGORITHM%/%date%/%PROB_DIR_NAME%/gauss_best_worst_{0}_%ALGORITHM%.csv -iterToBest=G:/dokumenty/magisterka-geo-szer/raport/%ALGORITHM%/%date%/%PROB_DIR_NAME%/iterations_to_best.csv -best=%BEST% -ri=%RUN_ITERATIONS% -i=%ITERATIONS% -prob=%PROB%  > .run/%ALGORITHM%_%date%_prob_%PROB_DIR_NAME%.out.txt
+java -jar target/scheduling-geo-0.0.1-SNAPSHOT-jar-with-dependencies.jar -f=src/main/resources/%ALGORITHM%.txt -bestMeanWorst=G:/dokumenty/magisterka-geo-szer/raport/%ALGORITHM%/%date%/%PROB_DIR_NAME%/iterations_best_%ALGORITHM%.csv -currentBestWorst=G:/dokumenty/magisterka-geo-szer/raport/%ALGORITHM%/%date%/%PROB_DIR_NAME%/gauss_best_worst_{0}_%ALGORITHM%.csv -iterToBest=G:/dokumenty/magisterka-geo-szer/raport/%ALGORITHM%/%date%/%PROB_DIR_NAME%/iterations_to_best.csv -distribution=G:/dokumenty/magisterka-geo-szer/raport/%ALGORITHM%/%date%/%PROB_DIR_NAME%/distribution.csv -best=%BEST% -ri=%RUN_ITERATIONS% -i=%ITERATIONS% -prob=%PROB%  > .run/%ALGORITHM%_%date%_prob_%PROB_DIR_NAME%.out.txt
