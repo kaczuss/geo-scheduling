ed1ad8a1d1bcd7b87185ecf2b6862cfd8a99be31
kaczuss <kaczan87@gmail.com>
2013-05-02 13:43:30 +0200

    dodatkowe metody statystyczne moda min max dominanta procent dominanty

--- src/main/java/pl/kaczanowski/analyze/StatisticsUtils.java
+++ src/main/java/pl/kaczanowski/analyze/StatisticsUtils.java
@@ -2,13 +2,58 @@ package pl.kaczanowski.analyze;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
-import java.math.BigDecimal;
 import java.util.Collection;
-import java.util.List;
+import java.util.Comparator;
+import java.util.Map.Entry;
+
+import org.apache.commons.math3.stat.Frequency;
+import org.apache.commons.math3.stat.StatUtils;
 
 import com.google.common.collect.Ordering;
+import com.google.common.primitives.Doubles;
+import com.google.common.primitives.Ints;
+import com.google.common.primitives.Longs;
 
 public final class StatisticsUtils {
+
+	private static final Comparator<Entry<? super Long, Long>> FRAQUANCE_COMPARATOR =
+			new Comparator<Entry<? super Long, Long>>() {
+
+				@Override
+				public int compare(final Entry<? super Long, Long> o1, final Entry<? super Long, Long> o2) {
+					int valuesCompare = Longs.compare(o1.getValue(), o2.getValue());
+					if (valuesCompare == 0) {
+						// zamieniona kolejnosc, wazniejsze sa te, ktore maja mniejsza wartosc
+						return -Longs.compare((Long) o1.getKey(), (Long) o2.getKey());
+					}
+					return valuesCompare;
+				}
+			};
+
+	public static Double getFirstQuartileValue(final Collection<Integer> values) {
+
+		checkNotNull(values, "List cannot be null");
+
+		if (values.isEmpty()) {
+			return null;
+		}
+		double[] doubleArray = toDoubleArray(values);
+
+		return StatUtils.percentile(doubleArray, 25);
+
+	}
+
+	private static Frequency getFrequance(final Collection<Integer> values) {
+		Frequency f = new Frequency();
+		for (Integer val : values) {
+			f.addValue(val);
+		}
+		return f;
+	}
+
+	public static Integer getMax(final Collection<Integer> values) {
+		return Ints.max(Ints.toArray(values));
+	}
 
 	public static Double getMean(final Collection<Integer> values) {
 
@@ -17,13 +62,8 @@ public final class StatisticsUtils {
 		if (values.isEmpty()) {
 			return null;
 		}
-
-		BigDecimal sum = BigDecimal.ZERO;
-		for (Integer val : values) {
-			sum = sum.add(BigDecimal.valueOf(val));
-		}
-
-		return sum.divide(BigDecimal.valueOf(values.size())).doubleValue();
+		double[] doubleArray = toDoubleArray(values);
+		return StatUtils.mean(doubleArray);
 
 	}
 
@@ -34,13 +74,71 @@ public final class StatisticsUtils {
 		if (values.isEmpty()) {
 			return null;
 		}
-		List<Integer> sorted = Ordering.natural().sortedCopy(values);
-		if (sorted.size() % 2 == 1) {
-			return Double.valueOf(sorted.get(sorted.size() / 2));
-		} else {
-			return (sorted.get(sorted.size() / 2) + sorted.get(sorted.size() / 2 - 1)) / 2.0;
-		}
+		double[] doubleArray = toDoubleArray(values);
 
+		return StatUtils.percentile(doubleArray, 50);
+
+	}
+
+	public static Integer getMin(final Collection<Integer> values) {
+		return Ints.min(Ints.toArray(values));
+	}
+
+	/**
+	 * Dominanta (moda).
+	 * @return
+	 */
+	public static Integer getMode(final Collection<Integer> values) {
+		checkNotNull(values, "List cannot be null");
+
+		if (values.isEmpty()) {
+			return null;
+		}
+		Frequency f = getFrequance(values);
+
+		return getMode(f);
+
+	}
+
+	private static int getMode(final Frequency f) {
+		return ((Long) Ordering.from(FRAQUANCE_COMPARATOR).max(f.entrySetIterator()).getKey()).intValue();
+	}
+
+	public static Double getModePtc(final Collection<Integer> values) {
+		checkNotNull(values, "List cannot be null");
+
+		if (values.isEmpty()) {
+			return null;
+		}
+		Frequency f = getFrequance(values);
+		int mode = getMode(f);
+		return f.getPct(mode);
+	}
+
+	public static Double getThirdQuartileValue(final Collection<Integer> values) {
+
+		checkNotNull(values, "List cannot be null");
+
+		if (values.isEmpty()) {
+			return null;
+		}
+		double[] doubleArray = toDoubleArray(values);
+
+		return StatUtils.percentile(doubleArray, 75);
+	}
+
+	public static Double getVariance(final Collection<Integer> values) {
+		checkNotNull(values, "List cannot be null");
+
+		if (values.isEmpty()) {
+			return null;
+		}
+		double[] doubleArray = toDoubleArray(values);
+		return StatUtils.variance(doubleArray);
+	}
+
+	private static double[] toDoubleArray(final Collection<Integer> values) {
+		return Doubles.toArray(values);
 	}
 
 	private StatisticsUtils() {
---
+++ src/test/java/pl/kaczanowski/analyze/test/StatisticsUtilsModeTest.java
@@ -0,0 +1,40 @@
+package pl.kaczanowski.analyze.test;
+
+import static com.google.common.collect.Lists.newArrayList;
+import static org.fest.assertions.Assertions.assertThat;
+
+import org.testng.annotations.Test;
+
+import pl.kaczanowski.analyze.StatisticsUtils;
+
+@Test
+public class StatisticsUtilsModeTest extends StatisticsUtilsTest {
+
+	public void shouldReturnMoodeValueOnElementsValues() {
+		assertThat(StatisticsUtils.getMode(newArrayList(3, 4, 3, 4, 3, 4, 5, 5, 5, 5, 5, 5, 32, 2, 1, 0))).isEqualTo(
+				5);
+	}
+
+	public void shouldReturnProperModePercentage() {
+		assertThat(
+				StatisticsUtils.getModePtc(newArrayList(3, 4, 3, 4, 3, 4, 5, 5, 5, 5, 5, 5, 32, 2, 1, 0, 5, 5, 5, 5)))
+				.isEqualTo(0.5, delta);
+	}
+
+	public void shouldReturnProperModePercentageSameElements() {
+		assertThat(StatisticsUtils.getModePtc(newArrayList(3, 4, 3, 4, 5))).isEqualTo(0.4, delta);
+	}
+
+	public void shouldReturnSingleElementOnSingleElementList() {
+		assertThat(StatisticsUtils.getMode(singleElementList)).isNotNull().isEqualTo(singleElementList.get(0));
+	}
+
+	public void shouldReturnSmallerValueOnFewSameValues() {
+		assertThat(StatisticsUtils.getMode(newArrayList(3, 4, 3, 4, 3, 4))).isEqualTo(3);
+	}
+
+	@Test(expectedExceptions = NullPointerException.class)
+	public void shouldThrowNullPointerOnNullList() {
+		StatisticsUtils.getMode(null);
+	}
+}
