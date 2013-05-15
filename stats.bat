d7649ac4ca3c91e9d2c313b4ead5304ecc5c59d0
kaczuss <kaczan87@gmail.com>
2013-05-15 22:00:49 +0200

    opis g18

--- stats.bat
+++ stats.bat
@@ -1,9 +1,9 @@
 REM set ALGORITHM=tree15
 REM set BEST=9
-set ALGORITHM=intree15
-set BEST=9
-REM set ALGORITHM=g18
-REM set BEST=46
+REM set ALGORITHM=intree15
+REM set BEST=9
+set ALGORITHM=g18
+set BEST=46
 REM set ALGORITHM=g18_8
 REM set BEST=24
 REM set ALGORITHM=g40
@@ -19,7 +19,7 @@ REM set BEST=44
 
 
 REM set BASE_REPORT_DIR="G:\dokumenty\magisterka-geo-szer\raport\%ALGORITHM%\bestInIteration\2013-05-06\400 iteracji"
-set BASE_REPORT_DIR=G:\dokumenty\magisterka-geo-szer\raport\wyniki\2013-05-02\%ALGORITHM%\2013-05-02
+set BASE_REPORT_DIR=G:\dokumenty\magisterka-geo-szer\raport\2013-05-02\%ALGORITHM%\2013-05-02
 REM set BASE_REPORT_DIR=G:\dokumenty\magisterka-geo-szer\raport\%ALGORITHM%\2013-05-05\proc4
 REM set BASE_REPORT_DIR=G:\dokumenty\magisterka-geo-szer\raport\%ALGORITHM%\2013-05-07\proc8
 REM set BASE_REPORT_DIR=G:\dokumenty\magisterka-geo-szer\raport\%ALGORITHM%\2013-05-07(4000)\proc8
