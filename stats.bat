75eed879bdb0e4988034e95b1a840f5fab9399c2
kaczuss <kaczan87@gmail.com>
2013-05-16 20:21:34 +0200

    opis gauss18 2 proc

--- stats.bat
+++ stats.bat
@@ -6,20 +6,21 @@ REM set ALGORITHM=g18
 REM set BEST=46
 REM set ALGORITHM=g18_8
 REM set BEST=24
-set ALGORITHM=g40
-set BEST=80
+REM set ALGORITHM=g40
+REM set BEST=80
 REM set ALGORITHM=g40_4
 REM set BEST=45
 REM set ALGORITHM=g40_8
 REM set BEST=33
-REM set ALGORITHM=gauss18
-REM set BEST=44
+set ALGORITHM=gauss18
+set BEST=44
 REM set ALGORITHM=gauss18_8
 REM set BEST=44
 
 
 REM set BASE_REPORT_DIR="G:\dokumenty\magisterka-geo-szer\raport\%ALGORITHM%\bestInIteration\2013-05-06\400 iteracji"
 set BASE_REPORT_DIR=G:\dokumenty\magisterka-geo-szer\raport\2013-05-02\%ALGORITHM%\2013-05-02
+REM set BASE_REPORT_DIR="G:\dokumenty\magisterka-geo-szer\raport\gauss18\bestInIteration\2013-05-06\400 iteracji"
 REM set BASE_REPORT_DIR=G:\dokumenty\magisterka-geo-szer\raport\%ALGORITHM%\2013-05-05\proc4
 REM set BASE_REPORT_DIR=G:\dokumenty\magisterka-geo-szer\raport\%ALGORITHM%\2013-05-07\proc8
 REM set BASE_REPORT_DIR=G:\dokumenty\magisterka-geo-szer\raport\%ALGORITHM%\2013-05-07(4000)\proc8
